61f7be7b31ed7aff6c7406a382f4be693e8fd424
Nina Z <zukowskanina1@gmail.com>
2021-03-26 23:38:47 +0100

    avl takie na pol gwizdka wsm, dodawanie wygląda w miarę, ale delicja to antydelicja bo słaba raczej, niby działa, ale jednak nie wsm, myślę, że to może być wina złych rotacji, ale to potem jeszcze będzie oglądane

---
+++ AVL_a_humble_try.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include"student.hpp"
+#include"try_AVL.hpp"
+
+int main(){
+    AVL<Student> sosna;
+    sosna.add( new Student(" ", " ",4));
+    sosna.add( new Student(" ", " ",2));
+    sosna.add( new Student(" ", " ",1));
+    sosna.add( new Student(" ", " ",3));
+    sosna.add( new Student("kot", "bonifacy",7));
+    sosna.add( new Student("kot", "bonifacy",8));
+    sosna.add( new Student("kot", "bonifacy",9));
+    sosna.add( new Student("kot", "bonifacy",10));
+    sosna.add( new Student("kot", "bonifacy",81));
+    sosna.add( new Student("kot", "bonifacy",55));
+    sosna.add( new Student("kot", "bonifacy",56));
+    /*sosna.add( new Student(" ", " ",5));
+    sosna.add( new Student(" ", " ",12));
+    sosna.add( new Student("pies", "jamnik",8));
+    sosna.add( new Student(" ", " ",6));
+    sosna.add( new Student(" ", " ",9));
+    sosna.searching(7);*/
+    sosna.displayBSF();
+    sosna.deletenode(56);
+    sosna.displayBSF();
+    printf("bobo");
+    sosna.deletenode(1);
+    sosna.displayBSF();
+    sosna.deletenode(3);
+    sosna.displayBSF();
+
+
+
+return 0;}
