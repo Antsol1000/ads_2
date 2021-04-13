996bfa38e8e4995494d760d617d041a094c78f6d
Nina Z <zukowskanina1@gmail.com>
2021-04-13 12:10:13 +0200

    teraz wszystko pieknie

--- final_balanced_bst/final_avl_bst_test.cpp
+++ final_balanced_bst/final_avl_bst_test.cpp
@@ -5,18 +5,29 @@
 #include<algorithm>
 #include"../headers/tree.hpp"
 
-const int SIZE_STEP = 2000;
-const int MAX_SIZE = 100000;
+const int SIZE_STEP = 5000;
+const int MAX_SIZE = 300000;
 const int NUM_OF_TESTS = MAX_SIZE / SIZE_STEP;
+
+double average_array(double arr[], int len){
+	double sum = 0;
+	int i=0;
+	while(i<len){
+		sum+=(double) arr[i];
+		i++;
+	}
+	return (double)sum/(double)len;
+}
 
 int main() {
     srand(time(0));
-
+    int h;
     // open file with results of trials
     std::ofstream wyniki;
-    wyniki.open("avl_bst_results.csv");
-    wyniki << "test_number;number_of_records;add_time;search_time;remove_time" << std::endl;
-
+    wyniki.open("AVL_individual.csv");
+   wyniki << "test_number;number_of_records;height;add_time;search_time;remove_time" << std::endl;
+    int runs = 10;
+	double time_array[3][runs];
 
     for (int test_number = 1; test_number <= NUM_OF_TESTS; test_number++) {
 
@@ -24,53 +35,70 @@ int main() {
         int SIZE = SIZE_STEP * test_number;
         std::string *ARRAY = new std::string[SIZE];
         std::ifstream file;
-        file.open("..//data.csv");
+        file.open("..//data_2.csv");
         for (int i = 0; i < SIZE; i++) {
             file >> ARRAY[i];
         }
         file.close();
-
+        for(int run=0; run<runs;run++){
         // create data structure
         BalancedBST tree;
         clock_t start_t, end_t;
 
         // add all elements and measure time
-        double add_time;
+
+        std::random_shuffle(ARRAY, ARRAY + SIZE);
+        for (int i = 0; i < SIZE - 5000; i++) {
+            tree.insert(new Student(ARRAY[i].substr(8, 12), ARRAY[i].substr(21, 12), std::stoi(ARRAY[i].substr(0, 7))));
+        }
         start_t = clock();
-        for (int i = 0; i < SIZE; i++) {
+        for (int i = SIZE - 5000; i < SIZE; i++) {
             tree.insert(new Student(ARRAY[i].substr(8, 12), ARRAY[i].substr(21, 12), std::stoi(ARRAY[i].substr(0, 7))));
         }
         end_t = clock();
-        add_time = (double) (end_t - start_t);
+        time_array[0][run] = (double) (end_t - start_t);
+
+
+        h = tree.print_height();
 
         // shuffle the array and measure time of finding elements
         std::random_shuffle(ARRAY, ARRAY + SIZE);
-        double search_time;
+
+        for (int i = 0; i < SIZE -5000; i++) {
+            tree.find(std::stoi(ARRAY[i].substr(0, 7)));
+        }
         start_t = clock();
-        for (int i = 0; i < SIZE; i++) {
+        for (int i = SIZE -5000; i < SIZE; i++) {
             tree.find(std::stoi(ARRAY[i].substr(0, 7)));
         }
         end_t = clock();
-        search_time = (double) (end_t - start_t);
+        time_array[1][run] = (double) (end_t - start_t);
+
+
 
         // shuffle the array and measure time of removing elements
         std::random_shuffle(ARRAY, ARRAY + SIZE);
-        double remove_time;
+
+        for (int i = 0; i < SIZE-5000; i++) {
+            tree.remove(std::stoi(ARRAY[i].substr(0, 7)));
+        }
         start_t = clock();
-        for (int i = 0; i < SIZE; i++) {
+        for (int i = SIZE-5000; i < SIZE; i++) {
             tree.remove(std::stoi(ARRAY[i].substr(0, 7)));
         }
         end_t = clock();
-        remove_time = (double) (end_t - start_t);
+        time_array[2][run] = (double) (end_t - start_t);
+
+
 
         // delete data structure and ARRAY with records
-        delete &tree;
+        }
         delete []ARRAY;
 
         // print results to the file
-        wyniki << test_number  << ";" << SIZE << ";" << add_time << ";" << search_time << ";" << remove_time << std::endl;
+        wyniki << test_number  << ";" << SIZE << ";" <<h<<";"<< average_array(time_array[0],runs) << ";" << average_array(time_array[1],runs) << ";" << average_array(time_array[2],runs) << std::endl;
 
-        std::cout << "test nr" << test_number << " zakonczony\n";
+        std::cout << "test nr" << test_number <<"wysokosc : "<<h<< "  " << SIZE << "   " << average_array(time_array[0],runs) << "   " << average_array(time_array[1],runs) << "   " << average_array(time_array[2],runs) << std::endl;
     }
 
     // close results file
--- final_list/final_list_test.cpp
+++ final_list/final_list_test.cpp
@@ -5,18 +5,29 @@
 #include<algorithm>
 #include"../headers/list.hpp"
 
-const int SIZE_STEP = 2000;
-const int MAX_SIZE = 50000;
+const int SIZE_STEP = 500;
+const int MAX_SIZE = 15000;
 const int NUM_OF_TESTS = MAX_SIZE / SIZE_STEP;
+
+double average_array(double arr[], int len){
+	double sum = 0;
+	int i=0;
+	while(i<len){
+		sum+=arr[i];
+		i++;
+	}
+	return sum/len;
+}
 
 int main() {
     srand(time(0));
-
+    int h;
     // open file with results of trials
     std::ofstream wyniki;
-    wyniki.open("list_results.csv");
-    wyniki << "test_number;number_of_records;add_time;search_time;remove_time" << std::endl;
-
+    wyniki.open("llist_indv.csv");
+   wyniki << "test_number;number_of_records;add_time;search_time;remove_time" << std::endl;
+    int runs = 7;
+	double time_array[3][runs];
 
     for (int test_number = 1; test_number <= NUM_OF_TESTS; test_number++) {
 
@@ -24,52 +35,66 @@ int main() {
         int SIZE = SIZE_STEP * test_number;
         std::string *ARRAY = new std::string[SIZE];
         std::ifstream file;
-        file.open("..//data.csv");
+        file.open("..//data_2.csv");
         for (int i = 0; i < SIZE; i++) {
             file >> ARRAY[i];
         }
         file.close();
-
+        for(int run=0; run<runs;run++){
         // create data structure
-        SortedList list;
+        SortedList tree;
         clock_t start_t, end_t;
 
         // add all elements and measure time
-        double add_time;
+
+        std::random_shuffle(ARRAY, ARRAY + SIZE);
+        for (int i = 0; i < SIZE - 500; i++) {
+            tree.insert(new Student(ARRAY[i].substr(8, 12), ARRAY[i].substr(21, 12), std::stoi(ARRAY[i].substr(0, 7))));
+        }
         start_t = clock();
-        for (int i = 0; i < SIZE; i++) {
-            list.insert(new Student(ARRAY[i].substr(8, 12), ARRAY[i].substr(21, 12), std::stoi(ARRAY[i].substr(0, 7))));
+        for (int i = SIZE - 500; i < SIZE; i++) {
+            tree.insert(new Student(ARRAY[i].substr(8, 12), ARRAY[i].substr(21, 12), std::stoi(ARRAY[i].substr(0, 7))));
         }
         end_t = clock();
-        add_time = (double) (end_t - start_t);
+        time_array[0][run] = (double) (end_t - start_t);
+
+
 
         // shuffle the array and measure time of finding elements
         std::random_shuffle(ARRAY, ARRAY + SIZE);
-        double search_time;
+
         start_t = clock();
-        for (int i = 0; i < SIZE; i++) {
-            list.find(std::stoi(ARRAY[i].substr(0, 7)));
+        for (int i = 0; i < 500; i++) {
+            tree.find(std::stoi(ARRAY[i].substr(0, 7)));
         }
         end_t = clock();
-        search_time = (double) (end_t - start_t);
+        time_array[1][run] = (double) (end_t - start_t);
+
+
 
         // shuffle the array and measure time of removing elements
         std::random_shuffle(ARRAY, ARRAY + SIZE);
-        double remove_time;
+
         start_t = clock();
-        for (int i = 0; i < SIZE; i++) {
-            list.remove(std::stoi(ARRAY[i].substr(0, 7)));
+        for (int i = 0; i < 500; i++) {
+            tree.remove(std::stoi(ARRAY[i].substr(0, 7)));
         }
         end_t = clock();
-        remove_time = (double) (end_t - start_t);
+        time_array[2][run] = (double) (end_t - start_t);
+        for (int i = 500; i < SIZE; i++) {
+            tree.remove(std::stoi(ARRAY[i].substr(0, 7)));
+        }
+
+
 
         // delete data structure and ARRAY with records
-        delete &list;
+        }
         delete []ARRAY;
 
         // print results to the file
-        wyniki << test_number  << ";" << SIZE << ";" << add_time << ";" << search_time << ";" << remove_time << std::endl;
-        std::cout << "test number " << test_number << std::endl;
+        wyniki << test_number  << ";" << SIZE <<";"<< average_array(time_array[0],runs) << ";" << average_array(time_array[1],runs) << ";" << average_array(time_array[2],runs) << std::endl;
+
+        std::cout << "test nr" << test_number << "  " << SIZE << "   " << average_array(time_array[0],runs) << "   " << average_array(time_array[1],runs) << "   " << average_array(time_array[2],runs) << std::endl;
     }
 
     // close results file
